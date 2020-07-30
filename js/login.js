b0d02666658183def56e177f16010b6e9033f265
sachin kalonia <sachin@farmart.co>
2020-07-30 15:06:02 +0530

    js realign

--- js/login.js
+++ js/login.js
@@ -1,18 +1,12 @@
-document.getElementById("login_btn").addEventListener("click",proceedLogin);
+var login_button = document.getElementById("login_btn");
+var checkInputs = function () {
+  var user_name = document.getElementById("user").value;
+  var password = document.getElementById("pass").value;
+  console.log(user_name + "" + password);
+};
 
+var proceedLogin = function () {
+  checkInputs();
+};
 
-var checkInputs = function()
-{
-    var user_name = document.getElementById("user").value;
-    var password = document.getElementById("pass").value;
-    console.log(user_name+""+password);
-}
-
-var proceedLogin = function()
-{
-    alert();
-    checkInputs();
-}
-
-
-
+login_button.addEventListener("click", proceedLogin);
